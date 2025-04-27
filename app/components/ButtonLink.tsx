fe8409fd9d2387fe200a5bd9467e349f9badd915
Arsalan Rabdanov <arsalan.rabdanov2078@gmail.com>
2025-04-27 14:21:03 -0400

    improved button underline

--- app/components/ButtonLink.tsx
+++ app/components/ButtonLink.tsx
@@ -12,7 +12,7 @@ export default function ButtonLink({
 }: ButtonLinkProps & React.AnchorHTMLAttributes<HTMLAnchorElement>) {
   return (
     <a
-      className={`text-rg inline-block leading-150 font-bold transition ease-out ${variation === "primary" ? "border-green bg-green hover:text-green border-2 px-400 py-150 text-purple-900 hover:bg-transparent" : "border-green hover:text-green border-b-[0.1875rem] text-white"}`}
+      className={`text-rg inline-block leading-150 font-bold transition ease-out ${variation === "primary" ? "border-green bg-green hover:text-green border-2 px-400 py-150 text-purple-900 hover:bg-transparent" : "decoration-green hover:text-green text-white underline decoration-[0.1875rem] underline-offset-[0.5rem]"}`}
       {...props}
     >
       {children}
